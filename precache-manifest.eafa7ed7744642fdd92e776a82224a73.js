ae91afd91f6585af5555cf0f5107d6de8a90bc97
Filipa Marta <filipa.marta@gmail.com>
2020-04-27 21:17:24 +0100

    Updates

---
+++ precache-manifest.eafa7ed7744642fdd92e776a82224a73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c92dc52fba863ab6ddbeb17831249f",
+    "url": "/memegenerator/index.html"
+  },
+  {
+    "revision": "411d00eb9e2060f498d8",
+    "url": "/memegenerator/static/css/main.84d4da32.chunk.css"
+  },
+  {
+    "revision": "495cff149cc9175824ee",
+    "url": "/memegenerator/static/js/2.5fc0277f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memegenerator/static/js/2.5fc0277f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "411d00eb9e2060f498d8",
+    "url": "/memegenerator/static/js/main.fa93968e.chunk.js"
+  },
+  {
+    "revision": "32734df1af3a18cbbdf3",
+    "url": "/memegenerator/static/js/runtime-main.5ff5262a.js"
+  },
+  {
+    "revision": "66ed434c17fcf5683a3c295ff67cb60a",
+    "url": "/memegenerator/static/media/memegenerator.66ed434c.svg"
+  }
+]);
